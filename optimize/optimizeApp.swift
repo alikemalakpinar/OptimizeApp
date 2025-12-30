33642f961380cdd63e7e443c034e319aa9799f2e
AICO Software & Design <alikemal.gs@hotmail.com>
2025-12-30 13:04:03 +0300

    Initial Commit

---
+++ optimize/optimizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  optimizeApp.swift
+//  optimize
+//
+//  Created by Ali kemal Akpinar on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct optimizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
